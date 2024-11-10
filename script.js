3ad6b2a4974810accbb7fda904d6155e178aee35
Johan Jubin <johanjubinoommen@gmail.com>
2024-11-11 00:31:09 +0300

    Login Changes from Johan

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 var i = 0;
 var images = [];
 var time = 3000;
+var loopCount = 0;
+var maxLoops = 100;
 
-//image list
-images[0] = 'images/1st-bg.jpg'; 
-images[1] = 'images/2nd-bg.jpg'; 
-images[2] = 'images/3rd-bg.jpg'; 
-images[3] = 'images/4th-bg.jpg'; 
-images[4] = 'images/5th-bg.jpg'; 
+// Image list
+images[0] = 'images/1st-bg.jpg';
+images[1] = 'images/2nd-bg.jpg';
+images[2] = 'images/3rd-bg.jpg';
+images[3] = 'images/4th-bg.jpg';
+images[4] = 'images/5th-bg.jpg';
 
-//change image
-function changeImg(){
+// Change image
+function changeImg() {
     document.slide.src = images[i];
     
-    if(i < images, 1000){
+    // Increment index and loop counter if needed
+    if (i < images.length - 1) {
         i++;
     } else {
-        i=0;
+        i = 0;
+        loopCount++;
     }
 
-    setTimeout("changeImg()", time)
+    // Continue changing images if loop count is below maxLoops
+    if (loopCount <= maxLoops) {
+        setTimeout(changeImg, time);
+    }
 }
+
+// Initialize the first image on page load
+window.onload = changeImg;
+
 
 window.onload = changeImg;
 /*<!--<img class='demo' src="images/1st-bg.jpg">--!/>*/
